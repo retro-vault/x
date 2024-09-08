d60234784cebefea355dc3e2380ad5d51b79b82f
Tomaz Stih <tomaz.stih@wischner.co.uk>
2024-09-08 13:58:18 +0100

    Added C++ source file reading functions.

--- xld/tests/main.cpp
+++ xld/tests/main.cpp
@@ -1,6 +1,10 @@
+// test_postrack.cpp
+
 #include <gtest/gtest.h>
 
-int main(int argc, char *argv[]) {
-    testing::InitGoogleTest(&argc,argv);
+#include "convention.h"
+
+int main(int argc, char **argv) {
+    ::testing::init_google_test(&argc, argv);
     return RUN_ALL_TESTS();
 }
